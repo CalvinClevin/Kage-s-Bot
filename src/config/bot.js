59f93fe2bb645fc88f86070510d2665547469443
CalvinClevin <calvinclevin1@gmail.com>
2026-07-16 12:34:32 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Yardım için /help",     // this is what people actually see
+        state: "Babamız Kage",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
